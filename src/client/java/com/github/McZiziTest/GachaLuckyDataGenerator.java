82a4f4a96989d28fa4a6fab517642302efb760ab
benoi <benoit.desangles@gmail.com>
2026-01-04 14:12:17 +0100

    initial commit

---
+++ src/client/java/com/github/McZiziTest/GachaLuckyDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.McZiziTest;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GachaLuckyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
